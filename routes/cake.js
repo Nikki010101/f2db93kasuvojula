4edac068e21bdf202e7d65253432f1b96f137bb3
Nikki010101 <111778589+Nikki010101@users.noreply.github.com>
2022-11-25 17:14:17 -0600

    n

--- routes/cake.js
+++ routes/cake.js
@@ -14,10 +14,11 @@ var router = express.Router();
 router.get('/', cake_controlers.cake_view_all_Page ); 
 //module.exports = router; 
 // GET request for one costume. 
-//router.get('/cake/:id', cake_controlers.cake_detail); 
+router.get('/cake/:id', cake_controlers.cake_detail); 
 router.get('/detail', cake_controlers.cake_view_one_Page);
 router.get('/update', cake_controlers.cake_update_Page); 
 router.get('/delete', cake_controlers.cake_delete_Page); 
-router.get('/create', cake_controlers.cake_create_Page);  
+router.get('/create', cake_controlers.cake_create_Page); 
+//router.get('/logout',  cake_controlers.cake_update_Page); 
 module.exports = router;
 /* GET detail costume page */ 
